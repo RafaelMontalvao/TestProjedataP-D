04eccb036a91e5d7091b80ff6dd876ebe73171da
Rafael Brito <rushbig@hotmail.com>
2026-03-16 16:02:35 -0300

    configurando banco de dados h2

--- backend/Industria/src/main/java/test/Projedata/Industria/model/Product.java
+++ backend/Industria/src/main/java/test/Projedata/Industria/model/Product.java
@@ -1,10 +1,14 @@
 package test.Projedata.Industria.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Data
+@Entity
+@Table(name="product")
 public class Product {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
