8e1833310fe66611f4f512212d35084502281382
Rafael Brito <rushbig@hotmail.com>
2026-03-17 15:18:03 -0300

    create project Vue+vuetify

---
+++ frontend/industriaFrontEnd/src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'unfonts.css'
+
+
+import App from './App.vue'
+import router from './router'
+
+const vuetify = createVuetify({
+  theme: {
+   defaultTheme: 'light' // Isso força o tema light como padrão
+  },
+  components,
+  directives,
+})
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
+
+
+
+
+
