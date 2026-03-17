9914210f92baae5f605c5de33f9541e2df646397
Rafael Brito <rushbig@hotmail.com>
2026-03-17 16:45:05 -0300

    create component containerDefault e and add icons mdi  in project

---
+++ frontend/industriaFrontEnd/src/components/ContainerDefault.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+ <v-container fluid class="pa-4"  style="height:100%; overflow-y:auto;">
+
+    <slot></slot>
+   
+  </v-container>
+
+</template>
--- frontend/industriaFrontEnd/src/pages/HomePage.vue
+++ frontend/industriaFrontEnd/src/pages/HomePage.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+import ContainerDefault from '@/components/ContainerDefault.vue';
+
+</script>
+
 <template>
 
+     <container-default>
     DashBoard
+    </container-default>
+
 </template>
